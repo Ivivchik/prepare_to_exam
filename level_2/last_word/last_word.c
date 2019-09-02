8ce04349bf7597e903602d2fcccadd530525c5d5
Ivan <egam456@mail.ru>
2019-09-02 20:05:40 +0300

    prepare_to_exam

--- level_2/last_word/last_word.c
+++ level_2/last_word/last_word.c
@@ -6,14 +6,23 @@
 /*   By: hkuhic <hkuhic@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/01 21:02:37 by hkuhic            #+#    #+#             */
-/*   Updated: 2019/09/01 21:47:28 by hkuhic           ###   ########.fr       */
+/*   Updated: 2019/09/02 20:04:21 by hkuhic           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 
-int	ft_strlen(char *str)
+void	ft_swap(char *a, char *b)
+{
+	char c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
+
+int		ft_strlen(char *str)
 {
 	int i;
 
@@ -23,30 +32,42 @@ int	ft_strlen(char *str)
 	return (i);
 }
 
-int	main(int ac, char **av)
+char	*ft_strrev(char *str)
 {
 	int i;
 	int j;
-	int c;
-	int k;
 
-	k = 0;
+	j = 0;
+	i = ft_strlen(str);
+	while (j < ft_strlen(str) / 2)
+	{
+		ft_swap(&str[j], &str[i - 1]);
+		j++;
+		i--;
+	}
+	return (str);
+}
+
+int		main(int ac, char **av)
+{
+	int		i;
+	int		j;
+	char	a[1000500];
+
+	j = 0;
 	if (ac == 2)
 	{
-		i = 0;
-		while (av[1][i]!= '\0' && (av[1][i] == '\n' || av[1][i] == '\t' || av[1][i] == ' '))
-			k++;
-		i = ft_strlen(av[1]);
-		i--;
-		while (i >= 0 && (av[1][i] == '\n' || av[1][i] == '\t' || av[1][i] == ' '))
+		i = ft_strlen(av[1]) - 1;
+		while (av[1][i] == '\n' || av[1][i] == '\t' || av[1][i] == ' ')
 			i--;
-		c = i;
 		while (av[1][i] != '\n' && av[1][i] != '\t' && av[1][i] != ' ')
-			i--;
-		j = i;
-		while ( k <= j && j <= c)
+			a[j++] = av[1][i--];
+		a[j] = '\0';
+		ft_strrev(a);
+		j = 0;
+		while (a[j] != '\0')
 		{
-			write(1, av[1], 1);
+			write(1, &a[j], 1);
 			j++;
 		}
 	}
