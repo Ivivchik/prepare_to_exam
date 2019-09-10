2c3753f06c6dd75392db9be5a39e1ee9a8675b19
Ivan <egam456@mail.ru>
2019-09-10 20:48:22 +0300

    prepare_to_exam

--- level_3/ft_range/ft_range.c
+++ level_3/ft_range/ft_range.c
@@ -6,12 +6,13 @@
 /*   By: hkuhic <hkuhic@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/07 22:29:34 by hkuhic            #+#    #+#             */
-/*   Updated: 2019/09/10 19:18:50 by hkuhic           ###   ########.fr       */
+/*   Updated: 2019/09/10 20:45:36 by hkuhic           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 int		*ft_range(int start, int end)
 {
@@ -42,13 +43,13 @@ int		main(int ac, char **av)
 	int	e;
 	int	s;
 	int	*a;
-	int	i;
 
 	(void)ac;
 	s = atoi(av[1]);
 	e = atoi(av[2]);
 	a = ft_range(s, e);
-	while (*a)
-		printf("%d\n", a[i++]);
+	printf("%d\n", *(a + 1));
+	while (*a != '\0')
+		printf("%d ", *a++);
 	return (0);
 }
