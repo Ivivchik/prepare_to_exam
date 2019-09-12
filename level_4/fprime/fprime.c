b35bad68085b05e5bd76509b69a12120fb9bfc81
Ivan <egam456@mail.ru>
2019-09-12 20:19:31 +0300

    prepare_to_exam

--- level_4/fprime/fprime.c
+++ level_4/fprime/fprime.c
@@ -3,58 +3,66 @@
 /*                                                        :::      ::::::::   */
 /*   fprime.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: hkuhic <hkuhic@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/09/12 01:08:53 by marvin            #+#    #+#             */
-/*   Updated: 2019/09/12 01:08:53 by marvin           ###   ########.fr       */
+/*   Created: 2019/09/12 18:48:53 by hkuhic            #+#    #+#             */
+/*   Updated: 2019/09/12 20:12:31 by hkuhic           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int it_is_prime(int n)
+int		it_is_prime(int n)
 {
-    int i;
+	int i;
 
-    i = 2;
-    while (i < n)
-    {
-        if (n % i == 0)
-            return (0);
-        i++;
-    }
-    return (i);
+	i = 2;
+	while (i < n)
+	{
+		if (n % i == 0)
+			return (0);
+		i++;
+	}
+	return (i);
 }
 
-int main(int ac, char **av)
+void	fprime(int n)
 {
-    int i;
-    int numb;
-    (void)ac;
-    i = 2;
-    numb = atoi(av[1]);
-    if (ac == 2)
-    {
-        while (numb != 0)
-        {
-            if (it_is_prime(i) != 0 && numb % i == 0)
-            {
-                if (numb / i == 1)
-                {
-                    printf("%d", i);
-                    break ;
-                }
-                else
-                {
-                    printf("%d*", i);
-                    numb /= i;
-                    i = 1;
-                }
-            }
-            i++;
-        }
-    }
-    printf("\n");
-    return (0);
+	int i;
+
+	i = 2;
+	while (n)
+	{
+		if (it_is_prime(i) != 0 && n % i == 0)
+		{
+			if (n / i == 1)
+			{
+				printf("%d", i);
+				break ;
+			}
+			else
+			{
+				printf("%d*", i);
+				n /= i;
+				i = 1;
+			}
+		}
+		i++;
+	}
+}
+
+int		main(int ac, char **av)
+{
+	int i;
+	int n;
+
+	n = atoi(av[1]);
+	i = 2;
+	if (ac == 2)
+	{
+		fprime(n);
+	}
+	printf("\n");
+	return (0);
 }
